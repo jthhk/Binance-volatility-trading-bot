3fe0c0ecb87ec9cef9aa8215850e4af7a3d7d10a
jthhk <31700188+jthhk@users.noreply.github.com>
2022-01-05 23:47:41 +0800

    reorg

--- download_tickers_list_from_edgesforledges.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-fechahora=`date +%Y-%m-%d_%H-%M-%S`
-
-wget -qO- http://edgesforledges.com/watchlists/download/binance/fiat/usdt/innovation-zone | sed 's/BINANCE\://g;s/USDT//g' > "tickers_INNOVATION_$fechahora.txt"
-echo 
-echo Download Completed...
